7f55170c839f66b27f7580a63c943454a0e655dc
Festus <166383313+hachtech01@users.noreply.github.com>
2025-08-31 15:20:31 +0200

    Add files via upload

---
+++ lib_screens_math_screen_Version2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class MathScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Placeholder for math challenges
+    return Scaffold(
+      appBar: AppBar(title: Text('Math Challenge Zone')),
+      body: Center(
+        child: Text('Math problems and logic puzzles go here.'),
+      ),
+    );
+  }
+}
