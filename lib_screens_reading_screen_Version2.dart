e34cb67bcf0d1bc79a16d5fe5931dd23db51d1fa
Festus <166383313+hachtech01@users.noreply.github.com>
2025-08-31 15:15:17 +0200

    Add files via upload

---
+++ lib_screens_reading_screen_Version2.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ReadingScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Placeholder for reading stories
+    return Scaffold(
+      appBar: AppBar(title: Text('Reading Library')),
+      body: Center(
+        child: Text('Story goes here (with read-aloud and comprehension checks).'),
+      ),
+    );
+  }
+}
